e9b84e8e5a9f22bf5ebbb4dc81e98eebaffabb5c
Alex Tavarez <alexjtavarez@tutanota.com>
2023-12-10 14:14:39 -0500

    Added a directory for this year's AoC

---
+++ 2023/xmas1/solution.py
@@ -0,0 +1,24 @@
+# CURRENT GRADE: F
+
+with open('input.txt', 'r') as caldoc:
+  caldoc = caldoc.readlines()
+
+  sum = 0
+  for entry in caldoc:
+    chrnum = 0
+    numeral_opts = ''
+    while chrnum < len(entry):
+      if ord(entry[chrnum]) > 47 and ord(entry[chrnum]) < 58:
+        numeral_opts += entry[chrnum]
+      else:
+        chrnum += 1
+        continue
+      chrnum += 1
+    
+    if numeral_opts:
+      number = int(numeral_opts[0] + numeral_opts[-1])
+      # if len(numeral_opts) > 1:
+      # else:
+      #   number = numeral_opts
+    sum += number
+  print(sum)
